c747fcc2949e53ca61cbe9fc1df5ad500d82255f
TanDao01262000 <tankhanhdao@gmail.com>
2023-07-31 10:30:44 -0700

    changed setting for static files

--- Question_Blueprint/Question_Blueprint/settings.py
+++ Question_Blueprint/Question_Blueprint/settings.py
@@ -138,9 +138,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
