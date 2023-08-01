76fa1206de23c14e61af022f5a26536e9676e7cc
TanDao01262000 <tankhanhdao@gmail.com>
2023-07-31 17:07:01 -0700

    mnmnm

--- Question_Blueprint/Question_Blueprint/settings.py
+++ Question_Blueprint/Question_Blueprint/settings.py
@@ -103,13 +103,25 @@ WSGI_APPLICATION = 'Question_Blueprint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Mynewpassword123:)',
+        'HOST': 'db.sguhsgtqkpzqaxwejnkp.supabase.co',  
+        'PORT': '5432', 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
